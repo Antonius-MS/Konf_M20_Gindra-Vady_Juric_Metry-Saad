d44fed780d0030586dfe1481a600d3cfb5021554
Valentina Juric <valentina.juric@edu.fh-joanneum.at>
2021-06-05 17:57:52 +0200

    expanded RegistrierkasseTest with Report Tests

--- src/test/java/at/fhj/iit/RegistrierkasseTest.java
+++ src/test/java/at/fhj/iit/RegistrierkasseTest.java
@@ -52,4 +52,112 @@ public class RegistrierkasseTest {
         String result = registrierkasse.buyDrink("Puntigamer", "Friday 04.06.2021");
         assertEquals("We don't have a product called Puntigamer!", result);
     }
+
+    @Test
+    @DisplayName("Testing report for non alcoholic drink sales")
+    void testReportNonAlcoholicDrinksSales() {
+        blackCoffee.setPrice(6); // 3€ for 1 black coffee that is 0.5L
+        blackCoffee.sell();
+        heineken.setPrice(10); // 2€ for 1 heineken that is 0.2L
+        heineken.sell();
+        aW.setPrice(3); // 1.2€ for 1 aW that is 0.4L
+        aW.sell();
+        registrierkasse.buyDrink("Schwarzer Kaffee", "Friday 04.06.2021");
+        registrierkasse.buyDrink("Schwarzer Kaffee", "Saturday 05.06.2021");
+        registrierkasse.buyDrink("Heineken", "Sunday 06.06.2021");
+        registrierkasse.buyDrink("Heineken", "Saturday 05.06.2021");
+        registrierkasse.buyDrink("aW", "Saturday 05.06.2021");
+        registrierkasse.buyDrink("aW", "Tuesday 01.06.2021");
+        assertEquals(8.4, registrierkasse.reportNonAlcoholicDrinksSales());
+    }
+    @Test
+    @DisplayName("Testing report for low alcoholic drink sales")
+    void testReportLowAlcoholicDrinks() {
+        blackCoffee.setPrice(6); // 3€ for 1 black coffee
+        blackCoffee.sell();
+        heineken.setPrice(10); // 2€ for 1 heineken that is 0.2L
+        heineken.sell();
+        aW.setPrice(5); // 1.2€ for 1 aW that is 0.4L
+        aW.sell();
+        registrierkasse.buyDrink("Schwarzer Kaffee", "Friday 04.06.2021");
+        registrierkasse.buyDrink("Schwarzer Kaffee", "Saturday 05.06.2021");
+        registrierkasse.buyDrink("Heineken", "Sunday 06.06.2021");
+        registrierkasse.buyDrink("Heineken", "Saturday 05.06.2021");
+        registrierkasse.buyDrink("aW", "Saturday 05.06.2021");
+        registrierkasse.buyDrink("aW", "Tuesday 01.06.2021");
+        assertEquals(4, registrierkasse.reportLowAlcoholicDrinks());
+    }
+    @Test
+    @DisplayName("Testing report for high alcoholic drink sales")
+    void testReportHighAlcoholicDrinks() {
+        blackCoffee.setPrice(6); // 3€ for 1 black coffee that is 0.5L
+        blackCoffee.sell();
+        heineken.setPrice(10); // 2€ for 1 heineken that is 0.2L
+        heineken.sell();
+        aW.setPrice(3); // 1.2€ for 1 aW that is 0.4L
+        aW.sell();
+        registrierkasse.buyDrink("Schwarzer Kaffee", "Friday 04.06.2021");
+        registrierkasse.buyDrink("Schwarzer Kaffee", "Saturday 05.06.2021");
+        registrierkasse.buyDrink("Heineken", "Sunday 06.06.2021");
+        registrierkasse.buyDrink("Heineken", "Saturday 05.06.2021");
+        registrierkasse.buyDrink("aW", "Saturday 05.06.2021");
+        registrierkasse.buyDrink("aW", "Tuesday 01.06.2021");
+        assertEquals(0.0, registrierkasse.reportHighAlcoholicDrinks());
+    }
+    @Test
+    @DisplayName("Testing report for drink sales on date")
+    void testReportDrinksOnDate() {
+        blackCoffee.setPrice(6); // 3€ for 1 black coffee that is 0.5L
+        blackCoffee.sell();
+        heineken.setPrice(10); // 2€ for 1 heineken that is 0.2L
+        heineken.sell();
+        aW.setPrice(3); // 1.2€ for 1 aW that is 0.4L
+        aW.sell();
+        registrierkasse.buyDrink("Schwarzer Kaffee", "Friday 04.06.2021");
+        registrierkasse.buyDrink("Schwarzer Kaffee", "Saturday 05.06.2021");
+        registrierkasse.buyDrink("Heineken", "Sunday 06.06.2021");
+        registrierkasse.buyDrink("Heineken", "Saturday 05.06.2021");
+        registrierkasse.buyDrink("aW", "Saturday 05.06.2021");
+        registrierkasse.buyDrink("aW", "Tuesday 01.06.2021");
+        assertEquals(6.2, registrierkasse.reportDrinksOnDate("Saturday 05.06.2021"));
+    }
+    @Test
+    @DisplayName("Testing report for drink sales from seller")
+    void testReportDrinksFromPerson() {
+        // Monday, Friday and Saturday Max Mustermann sells the drinks
+        // All other days Katrin Musterfrau sells the drinks
+        blackCoffee.setPrice(6); // 3€ for 1 black coffee that is 0.5L
+        blackCoffee.sell();
+        heineken.setPrice(10); // 2€ for 1 heineken that is 0.2L
+        heineken.sell();
+        aW.setPrice(3); // 1.2€ for 1 aW that is 0.4L
+        aW.sell();
+        registrierkasse.buyDrink("Schwarzer Kaffee", "Friday 04.06.2021");
+        registrierkasse.buyDrink("Schwarzer Kaffee", "Saturday 05.06.2021");
+        registrierkasse.buyDrink("Heineken", "Sunday 06.06.2021");
+        registrierkasse.buyDrink("Heineken", "Saturday 05.06.2021");
+        registrierkasse.buyDrink("aW", "Saturday 05.06.2021");
+        registrierkasse.buyDrink("aW", "Tuesday 01.06.2021");
+        assertEquals(3.2, registrierkasse.reportDrinksFromPerson("Katrin Musterfrau"));
+    }
+    @Test
+    @DisplayName("Testing report for drink sales on date and from seller")
+    void testReportDrinksFromPersonAndOnDate() {
+        // Monday, Friday and Saturday Max Mustermann sells the drinks
+        // All other days Katrin Musterfrau sells the drinks
+        blackCoffee.setPrice(6); // 3€ for 1 black coffee that is 0.5L
+        blackCoffee.sell();
+        heineken.setPrice(10); // 2€ for 1 heineken that is 0.2L
+        heineken.sell();
+        aW.setPrice(3); // 1.2€ for 1 aW that is 0.4L
+        aW.sell();
+        registrierkasse.buyDrink("Schwarzer Kaffee", "Friday 04.06.2021");
+        registrierkasse.buyDrink("Schwarzer Kaffee", "Saturday 05.06.2021");
+        registrierkasse.buyDrink("Heineken", "Sunday 06.06.2021");
+        registrierkasse.buyDrink("Heineken", "Saturday 05.06.2021");
+        registrierkasse.buyDrink("aW", "Saturday 05.06.2021");
+        registrierkasse.buyDrink("aW", "Tuesday 01.06.2021");
+        assertEquals(3.0, registrierkasse.reportDrinksFromPersonAndOnDate("Max Mustermann", "Friday 04.06.2021"));
+    }
+
 }
